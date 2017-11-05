e3d53bbb518e814ca42b8cdc6282ab756263ef95
lilde90 <leepan1990@gmail.com>
2017-11-05 23:45:02 +0800

    add scoped lock

--- yarpc/base/mutex_lock.h
+++ yarpc/base/mutex_lock.h
@@ -1,6 +1,7 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 // interface for mutex lock wrapper
+//
 #ifndef _YARPC_BASE_MUTEX_LOCK_H_
 #define _YARPC_BASE_MUTEX_LOCK_H_
 
@@ -13,12 +14,15 @@ public:
   MutexLock() {
     pthread_mutex_init(&lock, NULL);
   }
+
   ~MutexLock() {
     pthread_mutex_destroy(&_lock);
   }
+
   void lock() {
     pthread_mutex_lock(&_lock);
   }
+
   void unlock() {
     pthred_mutex_unlock(&_lock);
   }
---
+++ yarpc/base/scoped_lock.h
@@ -0,0 +1,30 @@
+// Copyright 2017 lilde90. All Rights Reserved.
+// Author: Pan Li (panli.me@gmail.com)
+// wrapper class for scoped lock
+
+#ifndef _YARPC_YARPC_SCOPED_LOCK_H_
+#define _YARPC_YARPC_SCOPED_LOCK_H_
+namespace yarpc {
+namespace base {
+template <typename Type>
+class ScopedLock {
+public:
+  ScopedLock (Type& lock) : _lock(lock) {
+    _lock->lock();
+  }
+
+  ScopedLock (Type* lock) : _lock(lock) {
+    _lock->lock();
+  }
+
+  ~ScopedLock() {
+    _lock->unlock();
+  }
+
+private:
+  Type* _lock;
+};
+
+} // namespace base
+} // namespace yarpc
+#endif // _YARPC_YARPC_SCOPED_LOCK_H_
