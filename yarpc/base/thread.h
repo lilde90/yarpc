b580dcf5919435245fadce0ad53b1d230f7005d3
lilde90 <leepan1990@gmail.com>
2017-11-18 21:21:39 +0800

    fix linux compile error

--- yarpc/base/thread.h
+++ yarpc/base/thread.h
@@ -5,11 +5,9 @@
 #ifndef _YARPC_BASE_THREAD_H_
 #define _YARPC_BASE_THREAD_H_
 
-//#include <yarpc/base/condition.h>
 #include <yarpc/base/mutex_lock.h>
 #include <yarpc/base/rw_lock.h>
 #include <yarpc/base/scoped_lock.h>
-//#include <yarpc/base/spin_lock.h>
 #include <yarpc/base/condition.h>
 #include <yarpc/base/blocking_queue.h>
 
@@ -17,6 +15,5 @@ class Thread {
 public:
   Thread();
 };
-
 
 #endif // _YACRPC_BASE_THREAD_H_
