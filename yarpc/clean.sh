ef14154c5624194dab65c08f41d0ab166dca3436
lilde90 <leepan1990@gmail.com>
2017-12-02 14:17:51 +0800

    add core sub folder

--- yarpc/clean.sh
+++ yarpc/clean.sh
@@ -3,21 +3,28 @@
 #!/bin/bash
 #
 
-rm -f CMakeCache.txt
-rm -rf CMakeFiles
-rm -f Makefile
-rm -f cmake_install.cmake
-
-rm -f ./base/CMakeCache.txt
-rm -rf ./base/CMakeFiles
-rm -f ./base/Makefile
-rm -f ./base/cmake_install.cmake
-rm -rf ./base/*.a
-
 if [ -f Makefile ]; then
   make clean
   cd base
   if [ -f Makefile ]; then
     make clean
   fi
+  cd ..
+  cd core
+  if [ -f Makefile ]; then
+    make clean
+  fi
+  cd ..
 fi
+
+rm -f CMakeCache.txt
+rm -rf CMakeFiles
+rm -f Makefile
+rm -f cmake_install.cmake
+
+rm -f ./*/CMakeCache.txt
+rm -rf ./*/CMakeFiles
+rm -f ./*/Makefile
+rm -f ./*/cmake_install.cmake
+rm -rf ./*/*.a
+
