3f8d2ad3ad4a634ea321513074eb033602b9a58d
lilde90 <leepan1990@gmail.com>
2017-12-03 15:51:01 +0800

    add event loop implementation

--- yarpc/core/event_loop.cpp
+++ yarpc/core/event_loop.cpp
@@ -1,3 +1,39 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+#include <yarpc/core/event_loop.h>
+#include <sys/types.h>
+#include <linux/unistd.h>
+#include <pthread.h>
+#include <yarpc/base/logging.h>
+
+namespace yarpc {
+namespace core {
+//__thread pid_t t_current_pid = getpid();
+using namespace yarpc::base;
+__thread EventLoop* t_thisThreadLoop = NULL;
+
+EventLoop::EventLoop()
+  :_looping(false) {
+
+  _thread_id = static_cast<pid_t>(::syscall(SYS_gettid));
+  LOG_TRACE("EventLoop created in thread %d", _thread_id);
+  if (t_thisThreadLoop == NULL) {
+    t_thisThreadLoop = this;
+  } else {
+    LOG_FATAL("A EventLoop has been created in thread %d", _thread_id);
+  }
+  
+}
+
+EventLoop::~EventLoop() {
+  if (_looping) {
+    t_thisThreadLoop = NULL;
+  }
+}
+
+EventLoop* EventLoop::getEventLoop() {
+  return t_thisThreadLoop;
+}
+} // namespace core
+} // namespace yarpc
--- yarpc/core/event_loop.h
+++ yarpc/core/event_loop.h
@@ -1,3 +1,30 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+#ifndef _YARPC_YARPC_CORE_H_
+#define _YARPC_YARPC_CORE_H_
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <linux/unistd.h>
+
+namespace yarpc {
+namespace core {
+class EventLoop {
+
+public:
+  EventLoop();
+
+  ~EventLoop();
+  void loop();
+  bool isInLoopThread() const;
+  static EventLoop* getEventLoop();
+private:
+  bool _looping;
+  pid_t _thread_id;
+};
+} // namespace core
+} // namespace yarpc
+
+#endif // _YARPC_YARPC_CORE_H_
