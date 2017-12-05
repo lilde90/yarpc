376d0b8509bdb0b60b8b604699db428c1982638b
lilde90 <leepan1990@gmail.com>
2017-12-05 11:24:38 -0500

    add socket socket_wrapper

---
+++ yarpc/core/socket.h
@@ -0,0 +1,18 @@
+// Copyright 2017 lilde90. All Rights Reserved.
+// Author: Pan Li (panli.me@gmail.com)
+//
+#ifndef _YARPC_YARPC_CORE_SOCKET_H_
+#define _YARPC_YARPC_CORE_SOCKET_H_
+
+
+namespace yarpc {
+namespace core {
+class Socket {
+private:
+  const int _fd;
+};
+
+} // namespace core
+} // namespace yarpc
+
+#endif // _YARPC_YARPC_CORE_SOCKET_H_
