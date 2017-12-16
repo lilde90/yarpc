848b8f4e1fcbc8171e527a5cc6c4e76f45616fff
lilde90 <leepan1990@gmail.com>
2017-12-16 23:39:31 +0800

    add address implementation

--- yarpc/core/address.cpp
+++ yarpc/core/address.cpp
@@ -1,3 +1,40 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+#include <yarpc/core/address.h>
+
+namespace yarpc {
+namespace core {
+
+Address::Address(uint16_t port) {
+}
+
+Address::Address(std::string ip, uint16_t port) {
+}
+
+Address::Address(const struct sockaddr_in& addr) {
+}
+
+std::string Address::toIp() const {
+  return "";
+}
+
+std::string Address::toIpPort() const {
+  return "";
+}
+
+uint16_t Address::toPort() const {
+  return 0;
+}
+
+uint32_t Address::ipNetEndian() const {
+  return 0;
+}
+
+bool resolve(std::string ip_address, Address* result) {
+  return false;
+}
+
+
+} // namespace core
+} // namespace yarpc
--- yarpc/core/address.h
+++ yarpc/core/address.h
@@ -1,3 +1,55 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+
+#ifndef _YARPC_YARPC_CORE_ADDRESS_H_
+#define _YARPC_YARPC_CORE_ADDRESS_H_
+
+#include <netinet/in.h>
+#include <string>
+#include <functional>
+#include <yarpc/core/socket.h>
+
+namespace yarpc {
+namespace core {
+
+class Address {
+public:
+  Address(uint16_t port);
+  Address(std::string ip, uint16_t port);
+
+  Address(const struct sockaddr_in& addr);
+
+
+  std::string toIp() const;
+  std::string toIpPort() const;
+  uint16_t toPort() const;
+
+  sa_family_t family() const {
+    return _addr.sin_family;
+  }
+
+  const struct sockaddr* getSockAddr() const {
+    return sockaddr_cast(&_addr);
+  }
+
+  void setSockAddr(const struct sockaddr_in& addr) {
+    _addr = addr;
+  }
+
+  uint32_t ipNetEndian() const;
+
+  uint16_t portNetEndian() const {
+    return _addr.sin_port;
+  }
+
+  static bool resolve(std::string ip_address, Address* result);
+
+private:
+  struct sockaddr_in _addr;
+};
+
+} // namespace core
+} // namespace yarpc
+
+#endif // _YARPC_YARPC_CORE_ADDRESS_H_
