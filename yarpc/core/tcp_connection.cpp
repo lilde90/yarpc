b2e8155f7640551653f9d7bd12ca3de0278df9c8
lilde90 <leepan1990@gmail.com>
2017-12-20 01:48:24 +0800

    fix commit

--- yarpc/core/tcp_connection.cpp
+++ yarpc/core/tcp_connection.cpp
@@ -1,3 +1,8 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+#include <yarpc/core/tcp_connection.h>
+namespace yarpc {
+namespace core {
+} // namespace core
+} // namespace yarpc
--- yarpc/core/tcp_connection.h
+++ yarpc/core/tcp_connection.h
@@ -1,3 +1,12 @@
 // Copyright 2017 lilde90. All Rights Reserved.
 // Author: Pan Li (panli.me@gmail.com)
 //
+#ifndef _YARPC_YARPC_CORE_TCP_CONNECTION_H_
+#define _YARPC_YARPC_CORE_TCP_CONNECTION_H_
+namespace yarpc {
+namespace core {
+class TcpConnection {
+};
+} // namespace core
+} // namespace yarpc
+#endif // _YARPC_YARPC_CORE_TCP_CONNECTION_H_
