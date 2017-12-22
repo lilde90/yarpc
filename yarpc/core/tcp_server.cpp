dde2eacb86b855aeb14098dddaa2e41c8a54df21
lilde90 <leepan1990@gmail.com>
2017-12-23 00:48:04 +0800

    implement tcp server

--- yarpc/core/tcp_server.cpp
+++ yarpc/core/tcp_server.cpp
@@ -2,7 +2,61 @@
 // Author: Pan Li (panli.me@gmail.com)
 //
 #include <yarpc/core/tcp_server.h>
+#include <yarpc/base/logging.h>
 namespace yarpc {
 namespace core {
+TcpServer::TcpServer(EventLoop* loop,
+    const Address& listen_addr,
+    const std::string& name):
+  _loop(loop),
+  _name(name),
+  _acceptor(new Acceptor(loop, listen_addr)),
+  _thread_pool(new EventLoopThreadPool(loop, _name)),
+  _next_conn_id(1){
+    _acceptor->setNewConnectionCallback(std::bind(&TcpServer::newConnection, this, std::placeholders::_1, std::placeholders::_2));
+  }
+
+TcpServer::~TcpServer() {
+  for (auto it = _connections.begin(); it !=
+      _connections.end(); ++it) {
+    TcpConnection* conn = it->second;
+    if (conn != NULL) {
+      delete conn;
+    }
+    //conn->getLoop()->runInLoop()
+  }
+  if (_loop != NULL) {
+    delete _loop;
+  }
+
+  if(_acceptor != NULL) {
+    delete _acceptor;
+  }
+}
+
+void TcpServer::setThreadNum(int num_threads) {
+  _thread_pool->setThreadNum(num_threads);
+}
+
+void TcpServer::start() {
+  _thread_pool->start(_thread_init_callback);
+  _loop->runInLoop(std::bind(&Acceptor::listen, _acceptor));
+}
+
+void TcpServer::newConnection(int sockfd, const Address& peer_addr) {
+  EventLoop* io_loop = _thread_pool->getNextLoop();
+  char buf[64];
+  snprintf(buf, sizeof(buf), "%s-%d", _name.c_str(), _next_conn_id);
+  ++_next_conn_id;
+  std::string conn_name = buf;
+
+  Address local_addr(yarpc::core::getLocalAddr(sockfd));
+  TcpConnection* conn = new TcpConnection(io_loop,
+      conn_name, sockfd, local_addr, peer_addr);
+  _connections[conn_name] = conn;
+  conn->setConnectionCallback(_connection_callback);
+  conn->setMessageCallback(_message_callback);
+}
+
 } // namespace core
 } // namespace yarpc
--- yarpc/core/tcp_server.h
+++ yarpc/core/tcp_server.h
@@ -3,9 +3,64 @@
 //
 #ifndef _YARPC_YARPC_CORE_TCP_SERVER_H_
 #define _YARPC_YARPC_CORE_TCP_SERVER_H_
+#include <string>
+#include <yarpc/base/yarpc_defs.h>
+#include <yarpc/core/event_loop.h>
+#include <yarpc/core/acceptor.h>
+#include <yarpc/core/address.h>
+#include <yarpc/core/event_loop_thread_pool.h>
+#include <yarpc/core/tcp_connection.h>
+#include <unordered_map>
+#include <functional>
 namespace yarpc {
 namespace core {
 class TcpServer {
+public:
+  typedef std::function<void(EventLoop*)> ThreadInitCallback;
+  TcpServer(EventLoop* loop,
+      const Address& listen_addr,
+      const std::string& name);
+  ~TcpServer();
+  const std::string& name() const {
+    return _name;
+  }
+  EventLoop* getLoop() const {
+    return _loop;
+  }
+  
+  void setThreadNum(int num_threads);
+  void setThreadInitCallback(const ThreadInitCallback& cb) {
+    _thread_init_callback = cb;
+  }
+
+  EventLoopThreadPool* threadPool() {
+    return _thread_pool;
+  }
+
+  void start();
+
+  void setConnectionCallback(const ConnectionCallback& cb) {
+    _connection_callback = cb;
+  }
+
+  void setMessageCallback(const MessageCallback& cb) {
+    _message_callback = cb;
+  }
+private:
+
+  EventLoop* _loop;
+  const std::string _name;
+  Acceptor* _acceptor;
+  EventLoopThreadPool* _thread_pool;
+  ConnectionCallback _connection_callback;
+  MessageCallback _message_callback;
+  ThreadInitCallback _thread_init_callback;
+  
+  int _started;
+  std::unordered_map<std::string, TcpConnection*> _connections;
+  int _next_conn_id;
+private:
+  void newConnection(int sockfd, const Address& peer_addr);
 };
 } // namespace core
 } // namespace yarpc
